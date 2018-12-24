1f55e841c1b5a790ab49c8c4d1e97e2e07073305
Dave Spencer <david.spencerian@gmail.com>
2018-12-24 14:59:57 -0500

    decently working

---
+++ code-fragments/continuous-mobilenet/index4.js
@@ -0,0 +1,140 @@
+
+
+const video = document.querySelector("#videoElement");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let  go_on = true;
+
+const pmap = new Map();
+
+if (navigator.mediaDevices.getUserMedia) {       
+    navigator.mediaDevices.getUserMedia({video: true})
+  .then(function(stream) {
+    video.srcObject = stream;
+  })
+  .catch(function(err0r) {
+    console.log("Something went wrong!");
+  });
+} 
+let hack;
+let last = new Date().getTime();
+let waiting = false;
+
+video.addEventListener('play', () => {
+  let tick = 0;
+
+  function step() {
+    if (! go_on) {
+      return;
+    }
+    requestAnimationFrame(step);
+
+    tick += 1;
+    var now = new Date().getTime();
+    if ((now - last) < 5000) {
+      return;
+    }
+    if (waiting) {
+      return;
+    }    
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    var myImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);    
+    console.log(tick, canvas.width, canvas.height, now-last, myImageData);
+
+    const img = tf.fromPixels(canvas).toFloat();
+
+    let t1 = new Date().getTime();
+    waiting = true;    
+    mobilenet.load().then(model => {
+        let t2 = new Date().getTime();        
+        model.classify(img).then(predictions => {
+            let t3 = new Date().getTime();
+            console.log('TIME', t3-t2, t2-t1);
+            showPredictions(predictions);
+            last = new Date().getTime();
+            waiting = false;
+          })});
+  }
+  
+  requestAnimationFrame(step);
+ }
+);
+
+
+function speak() {
+  speechSynthesis.speak(new SpeechSynthesisUtterance("hi"));
+}
+
+function showPredictions(predictions) {
+  hack = predictions;
+  console.log('Predictions: ');
+  console.log(predictions);
+  const bestClassName = predictions[0].className;
+  const bestProbability = Math.round(100.0 * predictions[0].probability);
+  let better = false;
+  let worse = false;
+  if (pmap.has(bestClassName)) {
+    var old = pmap.get(bestClassName);
+    if (old < bestProbability) {
+      console.log("BETTER", bestClassName, old, bestProbability);      
+      better = true;
+    } else {
+      worse = true;
+    }
+  }
+
+  for (var i = 0; i < 3; i++) {
+    const className = predictions[i].className;
+    const probability = Math.round(predictions[i].probability * 100.0);
+    document.getElementById("c" + i).innerHTML = className;
+    document.getElementById("p" + i).innerHTML = probability + "%";
+    document.getElementById("x" + i).value = probability;
+  }
+  if (worse) {
+    return;
+  }
+  // OK: better or new.
+
+  pmap.set(bestClassName, bestProbability);
+  if (better) {
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance("better " + bestClassName));
+  } else {
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance(bestClassName));
+  }
+
+  // Make class name shorter for display.
+  comma = bestClassName.indexOf(',');
+  if (comma > 0) {
+    cn = bestClassName.substring(0, comma);
+  } else{
+    cn = bestClassName;
+  }
+
+  const xid = "id-" + cn;
+  let update = true;
+  let newdiv = document.getElementById(xid);
+  if (newdiv == null) {
+    update = false;
+    console.log('new', xid);
+    newdiv = document.createElement("div");
+    newdiv.className = "x-div";
+    newdiv.id = xid;
+  } else {
+    console.log('update', xid);    
+    newdiv.innerHTML = "";
+  }
+  var newimg = document.createElement("img");
+  newimg.className = "x-img";
+  newimg.src = canvas.toDataURL();
+  newimg.width = newimg.height = 64;
+  newdiv.appendChild(newimg);
+  newdiv.appendChild(document.createElement("br"));
+  const text = document.createElement("span");
+  text.className = "x-text";  
+  text.appendChild(document.createTextNode(cn + "(" + bestProbability + "%)"));
+  newdiv.appendChild(text);
+  if (update) {
+  } else {
+    document.getElementById("images").appendChild(newdiv);
+  }
+}
